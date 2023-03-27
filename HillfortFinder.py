8effab16763af7652e32351b7cf853f65689d644
juergenlandauer <81024337+juergenlandauer@users.noreply.github.com>
2023-03-27 11:28:57 +0200

    Update HillfortFinder.py

--- HillfortFinder.py
+++ HillfortFinder.py
@@ -3,13 +3,15 @@ from fastai.vision.all import *
 
 from pathlib import Path
 
+mypath = Path(".")
+
 import streamlit as st
 
 class Predict:
     def __init__(self, filename):
-        st.write(Path().glob('**/*'))
-        st.write(str(Path/filename))
-        self.learn_inference = load_learner(Path()/filename)
+        st.write(mypath.glob('**/*'))
+        st.write(str(mypath/filename))
+        self.learn_inference = load_learner(mypath/filename)
         self.img = self.get_image_from_upload()
         if self.img is not None:
             self.prepare_output()
@@ -43,6 +45,9 @@ def process_output(self):
     st.write(f'process output')            
             
 if __name__=='__main__':
+    st.set_page_config(page_title="Hillfort Finder App")
+    st.header("Hillfort Finder App")
+
 
     modelfile = 'm_L100_0.pkl'
 
